27dfad721af4ef3d8750503c4fa85590ead55fc4
jeonhyun <jeonhyun@student.42seoul.kr>
2022-01-06 14:50:37 +0900

    iterator const added

--- cpp08/ex02/MutantStack.cpp
+++ cpp08/ex02/MutantStack.cpp
@@ -6,8 +6,11 @@
 /*   By: jeonhyun <jeonhyun@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 17:02:42 by jeonhyun          #+#    #+#             */
-/*   Updated: 2022/01/05 16:51:31 by jeonhyun         ###   ########.fr       */
+/*   Updated: 2022/01/06 14:29:26 by jeonhyun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+template<typename T>
+class MutantStack;
--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jeonhyun <jeonhyun@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 17:02:45 by jeonhyun          #+#    #+#             */
-/*   Updated: 2022/01/05 16:51:38 by jeonhyun         ###   ########.fr       */
+/*   Updated: 2022/01/06 14:40:29 by jeonhyun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,11 @@ template<typename T>
 class MutantStack : public std::stack<T>
 {
     public :
-        MutantStack(){};
+        MutantStack() : MutantStack<T>::stack() {};
         ~MutantStack(){};
-        MutantStack(const MutantStack &origin){
-            *this = origin;
-        };
-        MutantStack &operator=(const MutantStack &origin){
-            std::stack<T>::operator=(origin);
+        MutantStack(const MutantStack<T> &origin) : MutantStack<T>::stack(origin) {};
+        MutantStack &operator=(const MutantStack<T> &origin){
+            MutantStack<T>::stack::operator=(origin);
             return (*this);
         };
 
@@ -38,12 +36,18 @@ class MutantStack : public std::stack<T>
 
         iterator begin(){ return(this->c.begin()); };
         iterator end(){ return(this->c.end()); };
+        const_iterator begin() const { return(this->c.begin()); };
+        const_iterator end() const { return(this->c.end()); };
+        
         reverse_iterator rbegin() {return(this->rbegin());};
         reverse_iterator rend(){return(this->rend());};
-        const_iterator cbegin(){return (this->c.cbegin());};
-        const_iterator cend(){return (this->c.cend());};
-        const_reverse_iterator crbegin(){return (this->c.crbegin());};
-        const_reverse_iterator crend(){return (this->c.crend());};
+        const_reverse_iterator rbegin() const {return(this->rbegin());};
+        const_reverse_iterator rend() const {return(this->rend());};
+        
+        const_iterator cbegin() const {return (this->c.cbegin());};
+        const_iterator cend() const {return (this->c.cend());};
+        const_reverse_iterator crbegin() const {return (this->c.crbegin());};
+        const_reverse_iterator crend() const {return (this->c.crend());};
 };
 
 #endif
